b809640e5cce30df40fc9c401a85e3621e908a14
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-08-26 00:37:53 +0500

    chore: create the meeting modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,11 +4,15 @@ import Image from 'next/image';
 import React, { useState } from 'react'
 import HomeCard from './HomeCard';
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
-
+    
+    const createMeeting = () => {
+        
+    }
 
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -40,6 +44,15 @@ const MeetingTypeList = () => {
                 handleClick={() => setMeetingState('isJoiningMeeting')}
                 className="bg-yellow-1"
             />
+
+            <MeetingModal
+             isOpen={meetingState === 'isInstantMeeting'}
+             onClose={() => setMeetingState(undefined)}
+             title="Start an Instant Meeting"
+             className="text-center"
+             buttonText="Start Meeting"
+             handleClick={createMeeting}
+            /> 
         </section>
     )
 }
