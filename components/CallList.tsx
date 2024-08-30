0a1d4787c3eec67784a538d0b33c19b003ebdbc7
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-08-30 17:00:49 +0500

    chore: Import CallList component for displaying ended calls in previous and recordings pages

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,17 +1,20 @@
-// @ts-nocheck
-
 'use client';
 
-import { useGetCalls } from '@/hooks/useGetCalls'
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+
+import Loader from './Loader';
+import { useGetCalls } from '@/hooks/useGetCalls';
 import MeetingCard from './MeetingCard';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useToast } from './ui/use-toast';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
-    const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls();
     const router = useRouter();
+    const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+        useGetCalls();
     const [recordings, setRecordings] = useState<CallRecording[]>([]);
+    const { toast } = useToast();
 
     const getCalls = () => {
         switch (type) {
@@ -24,49 +27,91 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
             default:
                 return [];
         }
-    }
+    };
+
     const getNoCallsMessage = () => {
         switch (type) {
             case 'ended':
                 return 'No Previous Calls';
-            case 'recordings':
-                return 'No recordings';
             case 'upcoming':
                 return 'No Upcoming Calls';
+            case 'recordings':
+                return 'No Recordings';
             default:
                 return '';
         }
-    }
+    };
+
+    useEffect(() => {
+        const fetchRecordings = async () => {
+            try {
+                const callData = await Promise.all(
+                    callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
+                );
+
+                const recordings = callData
+                    .filter((call) => call.recordings.length > 0)
+                    .flatMap((call) => call.recordings);
+
+                setRecordings(recordings);
+            } catch (error) {
+                toast({ title: 'Try to refresh the page' });
+            }
+
+        };
+
+        if (type === 'recordings') {
+            fetchRecordings();
+        }
+    }, [type, callRecordings]);
+
+    if (isLoading) return <Loader />;
 
     const calls = getCalls();
     const noCallsMessage = getNoCallsMessage();
 
-
     return (
-        <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
-            {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording) => (
-                <MeetingCard
-                    key={(meeting as Call).id}
-                    icon={
-                        type === 'ended'
-                            ? '/icons/previous.svg'
-                            : type === 'upcoming'
-                                ? '/icons/upcoming.svg'
-                                : '/icons/recordings.svg'
-                    }
-                    title={(meeting as Call).state.custom.description.substring(0, 26) || 'No description'}
-                    date={meeting.state.startsAt.toLocaleString() || meeting.start_time.toLocaleString()}
-                    isPreviousMeeting={type === 'ended'}
-                    buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
-                    handleClick={type === 'recordings' ? () => router.push(`${meeting.url}`) : () => router.push(`/meeting/${meeting.id}`)}
-                    link={type === 'recordings' ? meeting.url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`}
-                    buttonText={type === 'recordings' ? 'Play' : 'Start'}
-                />
-            )) : (
-                <h1>{noCallsMessage}</h1>
+        <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+            {calls && calls.length > 0 ? (
+                calls.map((meeting: Call | CallRecording) => (
+                    <MeetingCard
+                        key={(meeting as Call).id}
+                        icon={
+                            type === 'ended'
+                                ? '/icons/previous.svg'
+                                : type === 'upcoming'
+                                    ? '/icons/upcoming.svg'
+                                    : '/icons/recordings.svg'
+                        }
+                        title={
+                            (meeting as Call).state?.custom?.description ||
+                            (meeting as CallRecording).filename?.substring(0, 20) ||
+                            'No Description'
+                        }
+                        date={
+                            (meeting as Call).state?.startsAt?.toLocaleString() ||
+                            (meeting as CallRecording).start_time?.toLocaleString()
+                        }
+                        isPreviousMeeting={type === 'ended'}
+                        link={
+                            type === 'recordings'
+                                ? (meeting as CallRecording).url
+                                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`
+                        }
+                        buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
+                        buttonText={type === 'recordings' ? 'Play' : 'Start'}
+                        handleClick={
+                            type === 'recordings'
+                                ? () => router.push(`${(meeting as CallRecording).url}`)
+                                : () => router.push(`/meeting/${(meeting as Call).id}`)
+                        }
+                    />
+                ))
+            ) : (
+                <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default CallList
+export default CallList;
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -109,19 +109,19 @@ const MeetingTypeList = () => {
                     <div className='flex flex-col gap-2.5'>
                         <label className='text-base text-normal leading-[22px] text-sky-2'>Add a description</label>
                         <Textarea className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
-                        onChange={(e) => setValues({...values, description: e.target.value})}
+                            onChange={(e) => setValues({ ...values, description: e.target.value })}
                         />
                     </div>
                     <div className='flex w-full flex-col gap-2.5'>
-                    <label className='text-base text-normal leading-[22px] text-sky-2'>Select Date and Time</label>
-                    <ReactDatePicker 
-                    selected={values.dateTime}
-                    showTimeSelect
-                    timeFormat='"HH:mm'
-                    timeIntervals={15}
-                    dateFormat="MMMM d, yyyy h:mm aa"
-                    className='w-full rounded bg-dark-3 p-2 focus:outline-none'
-                    />
+                        <label className='text-base text-normal leading-[22px] text-sky-2'>Select Date and Time</label>
+                        <ReactDatePicker
+                            selected={values.dateTime}
+                            showTimeSelect
+                            timeFormat='"HH:mm'
+                            timeIntervals={15}
+                            dateFormat="MMMM d, yyyy h:mm aa"
+                            className='w-full rounded bg-dark-3 p-2 focus:outline-none'
+                        />
                     </div>
                 </MeetingModal>
             ) : (
